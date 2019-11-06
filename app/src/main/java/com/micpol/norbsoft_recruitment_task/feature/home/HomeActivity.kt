7f4d24089c301a08b61c7c6bf5ecc64633d7cd64
Micpol <michal.polewczak@gmail.com>
2019-11-06 16:13:13 +0100

    Adds feature.home and renames MainActivity to HomeActivity

--- app/src/main/java/com/micpol/norbsoft_recruitment_task/feature/home/HomeActivity.kt
+++ app/src/main/java/com/micpol/norbsoft_recruitment_task/feature/home/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.micpol.norbsoft_recruitment_task
+package com.micpol.norbsoft_recruitment_task.feature.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.micpol.norbsoft_recruitment_task.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
