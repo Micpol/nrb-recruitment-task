ecb825ace9bb057970466b17c653d5c994d7690d
Micpol <michal.polewczak@gmail.com>
2019-11-06 17:57:14 +0100

    Observes channels liveData in fragment

--- app/src/main/java/com/micpol/norbsoft_recruitment_task/feature/home/HomeFragment.kt
+++ app/src/main/java/com/micpol/norbsoft_recruitment_task/feature/home/HomeFragment.kt
@@ -1,10 +1,10 @@
 package com.micpol.norbsoft_recruitment_task.feature.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.micpol.norbsoft_recruitment_task.R
 import com.micpol.norbsoft_recruitment_task.architecture.BaseFragment
@@ -18,21 +18,26 @@ class HomeFragment : BaseFragment() {
 
     private val viewModel: HomeVM by lazy { getVM<HomeVM>(viewModelFactory) }
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel.loadChannels()
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("HomeFragment", "onViewCreated: viewModel null ${viewModel == null}");
+        viewModel.channels.observe(
+            this,
+            Observer {
+                submitData()
+            })
+    }
+
+    private fun submitData() {
+
     }
 
 }
