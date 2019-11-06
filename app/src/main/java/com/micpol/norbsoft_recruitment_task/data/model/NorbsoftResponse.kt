74fc5f9908a12c1846282dbe8522a95531c908ea
Micpol <michal.polewczak@gmail.com>
2019-11-06 18:48:24 +0100

    Fixes models

--- app/src/main/java/com/micpol/norbsoft_recruitment_task/data/model/NorbsoftResponse.kt
+++ app/src/main/java/com/micpol/norbsoft_recruitment_task/data/model/NorbsoftResponse.kt
@@ -1,22 +1,28 @@
 package com.micpol.norbsoft_recruitment_task.data.model
 
 data class NorbsoftResponse(
-    val items: List<Channel> = listOf()
+    val items: List<Channel>
 )
 
 data class Channel(
-    val snippet: Snippet? = null
+    val snippet: Snippet?
 )
 
 data class Snippet(
-    val title: String = "",
-    val description: String = "",
-    val channelTitle: String = "",
+    val title: String,
+    val description: String,
+    val channelTitle: String,
+    val thumbnails: Thumbnails
+)
+
+data class Thumbnails(
+    val default: Thumbnail,
+    val medium: Thumbnail,
     val high: Thumbnail
 )
 
 data class Thumbnail(
-    val url: String = "",
+    val url: String,
     val width: Float = 0f,
     val height: Float = 0f
 )
